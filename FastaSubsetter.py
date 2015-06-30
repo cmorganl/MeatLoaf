9fb8dd92316b707849e35697d41b32db05b24507
Connor Morgan-Lang <c.morganlang@gmail.com>
2015-06-30 16:29:26 -0700

    Added a function to split on the ambiguiity character N, other improvements

--- FastaSubsetter.py
+++ FastaSubsetter.py
@@ -10,6 +10,8 @@ def set_arguments():
     parser.add_argument("-i", "--fasta", help = "The fasta file to be subsetted.", required=True)
     parser.add_argument("-l", "--list", help="The list of contig headers to be subsetted from the fasta file", required=True)
     parser.add_argument("-o", "--output", help="The output fasta file [DEFAULT = subset.fasta]", required=False, default="subset.fasta")
+    parser.add_argument("-N", "--Nsplit", help="Split the contigs on ambiguity character 'N's [DEFAULT = False]", \
+                        default=False, action="store_true")
     extract.add_argument("-s", "--slice", help="Flag indicating a specific sequence should be extracted from a specific header. \
                          The headers to be used for extraction will be pulled from the provided list", required=False, action="store_true")
     extract.add_argument("-p", "--pos", help="Positions (start,end) of the provided contig to extract", required=False)
@@ -27,7 +29,7 @@ def subset_fasta(fasta, headers):
         line = fas.readline()
         while line:
             if ( line[0] == '>' ):
-                header = line.strip()
+                header = line[1:].strip()
                 if header in headers:
                     subset[header] = ""
                     add = 1
@@ -44,7 +46,10 @@ def load_list(header_list):
     headers = list()
     with open(header_list) as LoH:
         for line in LoH:
-            headers.append(line.strip())
+            if ( line[0] == '>'):
+                headers.append(line[1:].strip())
+            else:
+                headers.append(line.strip())
     return headers
 
 def extract_seq(contig_seq, pos):
@@ -54,21 +59,50 @@ def extract_seq(contig_seq, pos):
     start, end = pos
     return str(contig_seq[start:end])
 
+def Nsplit_scaffolds(subset):
+    """
+    Splits all scaffolds containing 'N's on 'N' and
+    adds these contigs to the dictionary to be returned.
+    Removes all sub-contigs that are smaller than 200bp.
+    """
+    unambiguous_subset = dict()
+    acc = 1
+    for head in subset.keys():
+        if 'N' in subset[head]:
+            contigs = subset[head].split('N')
+            for c in contigs:
+                if len(c) > 200:
+                    contig_name = head + "_" + str(acc)
+                    acc += 1
+                    unambiguous_subset[contig_name] = c
+                    #sys.stderr.write(c)
+        else:
+            contig_name = head + "_" + str(acc)
+            acc += 1
+            unambiguous_subset[contig_name] = subset[head]
+    return unambiguous_subset
 
 def main():
+    print "Beginning", sys.argv[0]
     args = set_arguments()
     headers = load_list(args.list)
+    print "Finding the sequences in", args.list
     subset = subset_fasta(args.fasta, headers)
+    if args.Nsplit:
+        print "Splitting the sequences on 'N's..."
+        subset = Nsplit_scaffolds(subset)
+    ordered_subset = sorted(subset)
     if args.slice:
         with open(args.output, 'w') as fa_out:
-            for contig in subset:
+            for contig in ordered_subset:
                 seq = extract_seq(subset[contig], args.pos)
                 fa_out.write('>' + str(contig) + '\n')
                 fa_out.write(str(seq) + "\n")
     else:
+        print "Writing subset to", args.output
         with open(args.output, 'w') as fa_out:
-            for contig in subset:
-                fa_out.write('>' + str(contig) + '\n')
+            for contig in ordered_subset:
+                fa_out.write('>' + str(contig) + "\n")
                 fa_out.write(subset[contig] + "\n")
     return 0
 
