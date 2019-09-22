e3d3e386da85c8467b6c4ceabf68a79866e72610
Connor Morgan-Lang <c.morganlang@gmail.com>
2019-09-22 16:42:18 +0200

    Uses logging

--- JGI_GoLD_download.py
+++ JGI_GoLD_download.py
@@ -5,6 +5,7 @@ import sys
 import re
 import argparse
 import pycurl
+import logging
 import xml.etree.ElementTree as ET
 
 __author__ = 'Connor Morgan-Lang'
@@ -16,6 +17,92 @@ c = pycurl.Curl()
 # python3 -m pip install --compile --install-option="--with-nss" --no-cache-dir pycurl
 
 jgi_web = "https://genome.jgi.doe.gov/"
+
+
+class MyFormatter(logging.Formatter):
+
+    error_fmt = "%(levelname)s - %(module)s, line %(lineno)d:\n%(message)s"
+    warning_fmt = "%(levelname)s:\n%(message)s"
+    debug_fmt = "%(asctime)s\n%(message)s"
+    info_fmt = "%(message)s"
+
+    def __init__(self):
+        super().__init__(fmt="%(levelname)s: %(message)s",
+                         datefmt="%d/%m %H:%M:%S")
+
+    def format(self, record):
+
+        # Save the original format configured by the user
+        # when the logger formatter was instantiated
+        format_orig = self._style._fmt
+
+        # Replace the original format with one customized by logging level
+        if record.levelno == logging.DEBUG:
+            self._style._fmt = MyFormatter.debug_fmt
+
+        elif record.levelno == logging.INFO:
+            self._style._fmt = MyFormatter.info_fmt
+
+        elif record.levelno == logging.ERROR:
+            self._style._fmt = MyFormatter.error_fmt
+
+        elif record.levelno == logging.WARNING:
+            self._style._fmt = MyFormatter.warning_fmt
+
+        # Call the original formatter class to do the grunt work
+        result = logging.Formatter.format(self, record)
+
+        # Restore the original format configured by the user
+        self._style._fmt = format_orig
+
+        return result
+
+
+def prep_logging(log_file_name=None, verbosity=False):
+    """
+    Allows for multiple file handlers to be added to the root logger, but only a single stream handler.
+    The new file handlers must be removed outside of this function explicitly
+    :param log_file_name:
+    :param verbosity:
+    :return:
+    """
+    if verbosity:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    # Detect whether a handlers are already present and return if true
+    logger = logging.getLogger()
+    if len(logger.handlers):
+        return
+
+    formatter = MyFormatter()
+    # Set the console handler normally writing to stdout/stderr
+    ch = logging.StreamHandler()
+    ch.setLevel(logging_level)
+    ch.terminator = ''
+    ch.setFormatter(formatter)
+
+    if log_file_name:
+        output_dir = os.path.dirname(log_file_name)
+        try:
+            if output_dir and not os.path.isdir(output_dir):
+                os.makedirs(output_dir)
+        except (IOError, OSError):
+            logging.error("Unable to make directory '" + output_dir + "'.\n")
+            sys.exit(3)
+        logging.basicConfig(level=logging.DEBUG,
+                            filename=log_file_name,
+                            filemode='w',
+                            datefmt="%d/%m %H:%M:%S",
+                            format="%(asctime)s %(levelname)s:\n%(message)s")
+        logging.getLogger('').addHandler(ch)
+        logging.getLogger('').propagate = False
+    else:
+        logging.basicConfig(level=logging_level,
+                            datefmt="%d/%m %H:%M:%S",
+                            format="%(asctime)s %(levelname)s:\n%(message)s")
+    return
 
 
 def get_options():
@@ -40,7 +127,7 @@ def get_options():
         try:
             os.mkdir(args.output)
         except OSError:
-            sys.stderr.write("ERROR: unable to create new directory for output '" + args.output + "'.\n")
+            logging.error("Unable to create new directory for output '" + args.output + "'.\n")
             sys.exit(3)
 
     if args.output[-1] != os.sep:
@@ -59,30 +146,32 @@ def prep_and_curl(url_address, output_file, cookie_file):
     try:
         output_handler = open(output_file, 'wb')
     except IOError:
-        sys.stderr.write("ERROR: Unable to open xml file list " + output_file + " for writing!\n")
+        logging.error("Unable to open xml file list " + output_file + " for writing!\n")
         sys.exit(3)
 
     c.setopt(c.URL, url_address)
     c.setopt(c.COOKIEFILE, cookie_file)
     c.setopt(c.WRITEDATA, output_handler)
-    c.perform()
+    try:
+        c.perform()
+    except pycurl.error:
+        pass
 
     output_handler.close()
     return
 
 
 def dl_file_list(portal_name: str, cookies: str, file_xml: str) -> None:
-    sys.stdout.write("Downloading XML with listing all files... ")
-    sys.stdout.flush()
+    logging.info("Downloading XML with listing all files... ")
 
     if not os.path.isfile(cookies):
-        sys.stderr.write("ERROR: cookies file '" + cookies + "' doesn't exist!\n")
+        logging.error("Cookies file '" + cookies + "' doesn't exist!\n")
         sys.exit(3)
 
     page_address = jgi_web + "portal/ext-api/downloads/get-directory?organism=" + portal_name
     prep_and_curl(page_address, file_xml, cookies)
 
-    sys.stdout.write("done.\n")
+    logging.info("done.\n")
 
     return
 
@@ -92,7 +181,7 @@ def parse_files_xml(file_xml: str, ext=None) -> dict:
     try:
         xml_handler = open(file_xml, 'r')
     except IOError:
-        sys.stderr.write("ERROR: unable to open xml-file '" + file_xml + "' for reading!\n")
+        logging.error("Unable to open xml-file '" + file_xml + "' for reading!\n")
         sys.exit(3)
 
     tree = ET.parse(xml_handler)
@@ -134,8 +223,7 @@ def dl_files(dirs_n_files: dict, cookies, output: str, ext=None, target_dirs=Non
         if target_dirs and dir_name not in target_dirs:
             continue
 
-        sys.stdout.write("Downloading files in " + dir_name + "... ")
-        sys.stdout.flush()
+        logging.info("Downloading files in " + dir_name + "... ")
         dir_path = output + dir_name + os.sep
         if not os.path.isdir(dir_path):
             os.mkdir(dir_path)
@@ -148,10 +236,10 @@ def dl_files(dirs_n_files: dict, cookies, output: str, ext=None, target_dirs=Non
 
             prep_and_curl(file_address, file_path, cookies)
             num_dl += 1
-        sys.stdout.write("done.\n")
+        logging.info("done.\n")
 
     if num_dl == 0:
-        sys.stderr.write("ERROR: no files were downloaded.\n")
+        logging.error("No files were downloaded.\n")
         sys.exit(5)
 
     return
@@ -159,6 +247,9 @@ def dl_files(dirs_n_files: dict, cookies, output: str, ext=None, target_dirs=Non
 
 def main():
     args = get_options()
+    log_file = args.output + "JGI_downloader_log.txt"
+    prep_logging(log_file)
+
     file_list = args.output + args.name + "_files.xml"
     dl_file_list(args.name, args.cookies, file_list)
     addresses = parse_files_xml(file_list, args.extensions)
